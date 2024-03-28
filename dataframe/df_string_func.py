910e4f11d41ee1f0377512c2a7fd3d352dcd2adb
davidselvam <smilingdavid001@gmail.com>
2024-03-28 19:08:19 +0530

    practicing string function in DF

--- dataframe/df_string_func.py
+++ dataframe/df_string_func.py
@@ -1,20 +1,29 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import *
+from pyspark.sql.functions import *
 
 spark = SparkSession.builder.appName('Create dataframe ').getOrCreate()
-
-data = [('  david   ',' BSC CS'),
-        ('k7','MSC CS  '),
-        ('DiNagaran',' CSE'),
-        (' Deva Sellan','Cse'),
-        ('susai sebestiN','Bcom'),
-        ('JOhn ','BSC cs'),
-        ('chandru','B Tech'),
-        ('akash','M com')]
-schema = ['Name','Degree']
-
-df = spark.createDataFrame(data = data, schema = schema)
+df = spark.read.csv(path=r'C:\Users\AKASH.S\Documents\for_str_fun.csv',header=True,inferSchema=True)
 df.show()
-df.select('Name').show()
-df.select(df.Degree).show()
-df.select(df['Degree'],df['Name']).show()
+
+df.select('name').show()
+
+# using upper function
+
+uname = df.withColumn('Upper_name',upper(df['name']))
+uname.show()
+
+# using lower function
+
+lname = uname.withColumn('Lower_name',lower(uname['Upper_name']))
+lname.show()
+
+# using trim, rtrim and rtrim function in DF
+
+tname = lname.withColumn('trim_name',trim(lname['name']))\
+             .withColumn('right_trim',rtrim(lname['name']))\
+             .withColumn('left_trip',ltrim(lname['name']))
+tname.show()
+# substring function using df
+
+sub_string = df.withColumn('sub_string',substring(df['name'],2,5))
+sub_string.show()
