73c84eccb7daebdecdd5754ea6d1af99622465b8
davidselvam <smilingdavid001@gmail.com>
2024-03-31 03:36:00 +0530

    Readed JSON file and Performed Numeric Function

--- dataframe/read_json_numericFun.py
+++ dataframe/read_json_numericFun.py
@@ -15,34 +15,34 @@ schema = StructType([StructField('name', StringType(), True),
 
 df = spark.read.json(path=r'C:\Users\AKASH.S\Documents\Documents_DE\Datasets\employee.json',schema=schema)
 df.show()
-# df.printSchema()
-#
-# # numeric function
-#
-# # SUM FUNCTION
-#
-# df.select(sum(df.salary).alias('sum_salary')).show()
-# df.groupBy('city').agg(sum('salary').alias('total_salary')).show()
-#
-# # AVG FUNCTION
-#
-# df.select(avg(df.salary).alias('avg_salary')).show()
-# df.groupBy('department').agg(avg('salary').alias('avg_salary'),count('department').alias('count_dept')).show()
-# df.groupBy('department').agg(avg('salary').alias('avg_salary'),count('department').alias('count_dept')).filter(col('count_dept')>1).show()
-#
-# # MIN FUNCTION
-#
-# df.select(min('age').alias('min_age')).show()
-# df.groupBy('name').min('age').alias('min_age').show()
+df.printSchema()
 
-# MAX FUNCTION
-#
-# df.select(max(df.age)).show()
-#
-# # ROUND FUNCTION
-#
-# df.select(round(avg('salary'),3).alias('round_salary')).show()
+# numeric function
 
-# absolute function
+# SUM FUNCTION
+
+df.select(sum(df.salary).alias('sum_salary')).show()
+df.groupBy('city').agg(sum('salary').alias('total_salary')).show()
+
+# AVG FUNCTION
+
+df.select(avg(df.salary).alias('avg_salary')).show()
+df.groupBy('department').agg(avg('salary').alias('avg_salary'),count('department').alias('count_dept')).show()
+df.groupBy('department').agg(avg('salary').alias('avg_salary'),count('department').alias('count_dept')).filter(col('count_dept')>1).show()
+
+# MIN FUNCTION
+
+df.select(min('age').alias('min_age')).show()
+df.groupBy('name').min('age').alias('min_age').show()
+
+#MAX FUNCTION
+
+df.select(max(df.age)).show()
+
+# ROUND FUNCTION
+
+df.select(round(avg('salary'),3).alias('round_salary')).show()
+
+#absolute function
 
 df.select(abs(min('salary')).alias('NtoP')).show()
