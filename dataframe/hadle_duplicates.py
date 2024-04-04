e69c35f4a0f917639816709b3192a12f1e35e171
davidselvam <smilingdavid001@gmail.com>
2024-04-04 17:15:39 +0530

    Practicing to Handle Duplicates

--- dataframe/hadle_duplicates.py
+++ dataframe/hadle_duplicates.py
@@ -11,21 +11,22 @@ print('REMOVE DUPLICATE RPWS USING DISTINCT')
 distinct_df = df.distinct()
 distinct_df.show()
 
-# using dropduplicates() function
+# using dropduplicates() function remove the all column
 
+print('DROP DUPLICATES ALL COLUMNS : ')
 drop_duplicate = df.dropDuplicates()
 drop_duplicate.show()
 
 # specify one column in dropduplicates function
 
-print('DROP DUPLICATE USING ONE COLUMN : ')
+print('DROP DUPLICATE USING SPECIFIC COLUMN : ')
 drop_duplicate_one_col = df.dropDuplicates(['age'])
 drop_duplicate_one_col.show()
 
 # drop_duplicate function using multiple column
 
 print('DROP DUPLICATE USING MULTIPLE COLUMN :')
-drop_duplicate_mul_col = df.dropDuplicates(['name','gender'])
+drop_duplicate_mul_col = df.dropDuplicates(['name','gender','age'])
 drop_duplicate_mul_col.show()
 
 df.dropDuplicates(['age','name','id']).show()
